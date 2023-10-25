1ab9cf6e4902ba7c0f08364eab924d0f334e74b6
Jona T. Feucht <kontakt@jonafeucht.de>
2023-10-25 11:14:25 +0200

    fix

--- lib/sensitive_content_analysis.dart
+++ lib/sensitive_content_analysis.dart
@@ -1,1 +1,43 @@
-export 'sensitive_content_analysis_platform_interface.dart';
+import 'package:flutter/services.dart';
+
+const methodChannel = MethodChannel('sensitive_content_analysis');
+
+class SensitiveContentAnalysis {
+  /// Analyzes an image for sensitive content.
+  ///
+  /// Returns a `bool` indicating whether the image is sensitive.
+  Future<bool?> analyzeImage(Uint8List file) async {
+    try {
+      final dynamic isSensitive =
+          await methodChannel.invokeMethod('analyzeImage', file);
+      return isSensitive;
+    } on PlatformException catch (e) {
+      throw UnimplementedError(e.message);
+    }
+  }
+
+  /// Analyzes an network image for sensitive content.
+  ///
+  /// Returns a `bool` indicating whether the image is sensitive.
+  Future<bool?> analyzeNetworkImage({required String url}) async {
+    try {
+      final dynamic isSensitive =
+          await methodChannel.invokeMethod('analyzeNetworkImage', {"url": url});
+      return isSensitive;
+    } on PlatformException catch (e) {
+      throw UnimplementedError(e.message);
+    }
+  }
+
+  /// Check Policy.
+  ///
+  /// Returns a `int` indicating the policy value.
+  Future<int?> checkPolicy() async {
+    try {
+      final result = await methodChannel.invokeMethod('checkPolicy');
+      return result;
+    } on PlatformException catch (e) {
+      throw UnimplementedError(e.message);
+    }
+  }
+}
