ba6e258b59699518b615a64ef7b334f53ef2c0ff
Jona T. Feucht <14951074+jonafeucht@users.noreply.github.com>
2026-06-03 11:23:08 +0200

    feat: Swift Package Manager support

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import file_selector_macos
 import path_provider_foundation
 import sensitive_content_analysis
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SensitiveContentAnalysisPlugin.register(with: registry.registrar(forPlugin: "SensitiveContentAnalysisPlugin"))
